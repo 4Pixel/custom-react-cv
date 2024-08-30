55f6d6318a82ce79e9216b829f543360ff4ce9d0
Markus Glatz <markus.glatz@jollydays.com>
2024-08-30 20:37:02 +0200

    add manifest

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import { useRouter } from 'next/router'
 import 'public/app.css'
 
@@ -10,6 +11,10 @@ export default function MyApp({ Component, pageProps }) {
 
   return (
     <div>
+      <Head>
+        <link rel='manifest' href='/manifest.json' />
+        <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+      </Head>
       <Component {...pageProps} />
     </div>
   )
